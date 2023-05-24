de534347926a96f72402c19d3bb977d929267506
Ayoubbkm <ayoubbkm@gmail.com>
2023-05-24 12:41:43 +0100

    Folder

---
+++ src/common/NavBar.jsx
@@ -0,0 +1,18 @@
+import './styles/common/PanelBar.css'
+
+
+function Register(){
+    return(
+        <>
+        <div>
+        
+
+
+        </div>
+
+        </>
+    )
+}
+
+
+export default PanelBar

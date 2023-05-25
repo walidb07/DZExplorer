157d6ae656fdc837239e9ff6c0c600d323191460
Ayoubbkm <ayoubbkm@gmail.com>
2023-05-25 15:06:42 +0100

    Perfectionism Touches

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -9,23 +9,22 @@ function Admin(){
     return(
         <>
         <img className='background-admin' src='src\images\Admin\BackgroundAdmin.png'/>
-        <div className='admin-label'>Admin</div>
         <div className='admin-login-wrap'>
             <img className='admin-logo' src='src\images\Admin\LOGO.png'/>
             <div className='admin-field'>
-                <img className='' src='src\images\Admin\VectorAccount.svg'/>
+                <img className='account-img' src='src\images\Admin\VectorAccount.svg'/>
                 <input className='admin-input' type="text" placeholder="E-mail"/>
                 
             </div>
             <div className='admin-field'>
-                <img className='' src='src\images\Admin\VectorPassword.png'/>
+                <img className='password-img' src='src\images\Admin\VectorPassword.svg'/>
                 <input className='admin-input' type="password" placeholder="Mot de passe"/>
 
             </div>
             <button>
                 Connexion
             </button>
-            <a className='link'>Mot de passe oublié?</a>
+            <a className='link' href='#'>Mot de passe oublié?</a>
         </div>
         </>
     )

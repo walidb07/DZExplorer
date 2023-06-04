7f4c213f72a94c92c7d75fd366acb9d61aeea938
Walid Bouhedda <120842984+walidb07@users.noreply.github.com>
2023-06-04 12:29:28 +0100

    login axios

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,43 +1,74 @@
 import './styles/Login.css'
 import React, { useState } from 'react';
+import axios from 'axios';
 
 function TextField() {
   const [text, setText] = useState('');
 
   const handleChange = (event) => {
     setText(event.target.value);
- }
+  }
 }
 
-function Login(){
-    return(
-        <>
-        <div className='root-login'>
-            <div className="left-image">
-                <img src="src/images/Register-Login/Group 31.png" alt=""></img>
-            </div>
-            <div className='right-UI-login'>
-                <img src="src/images/Register-Login/Group 32.png" alt=""></img>
-                <h1>Se connecter</h1>
-                <div className='green-line'></div>
-                <p>Connectez-vous à votre compte pour continuer</p>
-                <div className="grid-container-login">
-                    <div className="grid-item-login">
-                        <h3>Email</h3>
-                        <input className='login-textbox' type="text" placeholder="Entrez votre email"/>
-                    </div>
-                    <div className="grid-item-login">
-                        <h3>Mot de passe</h3>
-                        <input className='login-textbox' type="password" placeholder="Entrez votre mot de passe"/>
-                    </div>
-                </div>
-                <button className="register-button">Se connecter</button>
-                <p>Vous n'avez pas de compte?</p>
-                <a className="connection-link" href="/register">S'inscrire</a>
-            </div>
+function Login() {
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleLogin = () => {
+    const name = "null";
+    const firstName = null;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const phoneNumber = null;
+    const country = "null";
+
+      axios.get('http://127.0.0.1:8000/Visiteur/', {
+        email: "walidforfour@gmail.com",
+        motDePasse: "viggen"
+        })
+        .then(response => {
+        if (response.data === 'sd') {
+            setErrorMessage('Compte existe déja');
+        } else if (response.data === 'error') {
+            setErrorMessage('Une erreur s\'est produite');
+        } else {
+            setErrorMessage('');
+        }
+
+        }).catch(error => {
+            
+        });
+    
+  };
+
+  return (
+    <>
+      <div className='root-login'>
+        <div className="left-image">
+          <img src="src/images/Register-Login/Group 31.png" alt=""></img>
         </div>
-        </>
-    )
+        <div className='right-UI-login'>
+          <img src="src/images/Register-Login/Group 32.png" alt=""></img>
+          <h1>Se connecter</h1>
+          <div className='green-line'></div>
+          <p>Connectez-vous à votre compte pour continuer</p>
+          <div className="grid-container-login">
+            <div className="grid-item-login">
+              <h3>Email</h3>
+              <input className='login-textbox' id="email" type="text" placeholder="Entrez votre email" />
+            </div>
+            <div className="grid-item-login">
+              <h3>Mot de passe</h3>
+              <input className='login-textbox' id="password" type="password" placeholder="Entrez votre mot de passe" />
+            </div>
+          </div>
+          {errorMessage && <p className='error-message'>{errorMessage}</p>}
+          <button className="register-button" onClick={handleLogin}>Se connecter</button>
+          <p>Vous n'avez pas de compte?</p>
+          <a className="connection-link" href="/register">S'inscrire</a>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Login
+export default Login;

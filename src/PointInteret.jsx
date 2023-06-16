d72f1d03e3f7a33e1bbc5cdf8227590b55860072
Walid Bouhedda <120842984+walidb07@users.noreply.github.com>
2023-06-16 15:04:13 +0100

    update PI

--- src/PointInteret.jsx
+++ src/PointInteret.jsx
@@ -3,15 +3,22 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBus } from '@fortawesome/free-solid-svg-icons'
 import { faClock } from '@fortawesome/free-solid-svg-icons'
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 function PointInteret() {
     return (
         <>
             <div className="PIpageRoot">
                 <div className="gallery"></div>
-                <div className="titre">Titre</div>
+                <div className="titreandrating">
+                    <div className='titre'>Titre</div>
+                    <div className='rating'>4.56 <FontAwesomeIcon icon={faStar} style={{color: "#000000",}} /></div>
+                </div>
                 <div className="carteName"><FontAwesomeIcon icon={faLocationDot} style={{color: "#000000",}} /> Lieu</div>
-                <div className="tags">Categories</div>
+                <div className="tags">
+                    <div className='tagBox'>Catégorie: </div>
+                    <div className='tagBox'>Thème: </div>
+                </div>
                 <div className="description">
                     <h3>Description</h3>
                     <p>La description du point d'intéret</p>

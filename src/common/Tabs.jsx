012fc4c94b1587353962df28c0a93b6cff33a6a3
Ayoubbkm <ayoubbkm@gmail.com>
2023-06-17 16:22:27 +0100

    changes

--- src/common/Tabs.jsx
+++ src/common/Tabs.jsx
@@ -6,49 +6,58 @@ import Recommendations from "./Recommendations.jsx"
 import './Dashboard.css'
 import Carte from '../Carte';
 import Actualities from './Actualities';
+import Event from './Event'; // Import the Event component
 
-
-function  Dashboard() {
-    return (
-        <div className='right-section'>
-          <div className="head-dash">
-          <HeadDash />
-          </div>
-            <div className="recommend">
-            <Recommendations />
-          </div>
+function Dashboard() {
+  return (
+    <div className='right-section'>
+      <div className="head-dash">
+        <HeadDash />
       </div>
-    )
-}
-function  DashboardCarte() {
-  return (
-      <div className='right-section'>
-        <div className="head-dash">
-        <HeadDash />
-        </div>
-          <div className="recommend-carte">
-          <Carte />
-        </div>
+      <div className="recommend">
+        <Recommendations />
+      </div>
     </div>
-  )
+  );
 }
-function  DashboardNews() {
+
+function DashboardCarte() {
   return (
-      <div className='right-section'>
-        <div className="head-dash">
+    <div className='right-section'>
+      <div className="head-dash">
         <HeadDash />
-        </div>
-          <div className="recommend-news">
-          <Actualities />
-        </div>
+      </div>
+      <div className="recommend-carte">
+        <Carte />
+      </div>
     </div>
-  )
+  );
 }
+
+function DashboardNews({ onActualityClick }) {
+  return (
+    <div className='right-section'>
+      <div className="head-dash">
+        <HeadDash />
+      </div>
+      <div className="recommend-news">
+        <Actualities onActualityClick={onActualityClick} />
+      </div>
+    </div>
+  );
+}
+
 function Tabs() {
   const [activeTab, setActiveTab] = useState('Accueil');
+  const [selectedActuality, setSelectedActuality] = useState(null);
 
   const handleButtonClick = (tab) => {
     setActiveTab(tab);
+  };
+
+  const handleActualityClick = (actuality) => {
+    setSelectedActuality(actuality);
+    setActiveTab('Event');
   };
 
   const renderContent = () => {
@@ -58,7 +67,9 @@ function Tabs() {
       case 'Carte':
         return <DashboardCarte />;
       case 'Actualités':
-        return <DashboardNews />;
+        return <DashboardNews onActualityClick={handleActualityClick} />;
+      case 'Event':
+        return <Event actuality={selectedActuality} />;
       default:
         return <Dashboard />;
     }
@@ -91,16 +102,19 @@ function Tabs() {
               >
                 Actualités
               </button>
-              <a href='/'>
-              <button className={activeTab === 'Se déconnecter' ? 'tab-active' : 'tab'}
-              >
-                Se déconnecter
-              </button>
+              <a href="/">
+                <button className={activeTab === 'Se déconnecter' ? 'tab-active' : 'tab'}>
+                  Se déconnecter
+                </button>
               </a>
             </div>
             <Calendar />
           </div>
-          {renderContent()}
+          {activeTab === 'Event' ? (
+            <Event actuality={selectedActuality} />
+          ) : (
+            renderContent()
+          )}
         </div>
       </div>
     </>

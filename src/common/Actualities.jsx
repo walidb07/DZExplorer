36dd104bb3f8f639c41b0e4c306c25c37f4f51df
Ayoubbkm <ayoubbkm@gmail.com>
2023-06-17 15:00:53 +0100

    problem fixed

--- src/common/Actualities.jsx
+++ src/common/Actualities.jsx
@@ -27,36 +27,39 @@ export default function Actualities() {
       description: 'Description description description',
       image: 'src/images/Act_4.png',
     },
-    {
-      location: 'Tlemcen',
-      title: 'Festival de la musique symphonique',
-      description: 'Description description description',
-      image: 'src/images/Act_5.png',
-    }
+    // Add more actualities here
   ];
 
+  const maxDisplayedItems = 3; // Maximum number of actualities to display
+  const displayActualities = actualities.slice(0, maxDisplayedItems); // Slice the actualities array based on the limit
+
   return (
-    <div>
-      <span className="relative">
-          <span className='ml-10 -mt-5 font-poppins font-bold text-xl'>Actualités</span>
-          <span className="absolute bottom-0 right-1/2 w-1/5 border-b border-green-500 border-xl"></span>
+    <>
+          <span className="relative">
+        <span className="ml-10 -mt-5 font-poppins font-bold text-xl">Actualités</span>
+        <span className="absolute bottom-0 right-1/2 w-1/5 border-b border-green-500 border-xl"></span>
       </span>
+    <div className="actualities-container">
+
       <div className="underline"></div>
 
-      {actualities.map((actuality, index) => (
-        <div className="one-actuality" key={index}>
-          <div className="one-actuality-info">
-            <div>
-              <span className="oa-location">{actuality.location}</span>
+      <div className="actualities-list">
+        {displayActualities.map((actuality, index) => (
+          <div className="one-actuality" key={index}>
+            <div className="one-actuality-info">
+              <div>
+                <span className="oa-location">{actuality.location}</span>
+              </div>
+              <a href="#" className="oa-title">
+                {actuality.title}
+              </a>
+              <p className="oa-desc">{actuality.description}</p>
             </div>
-            <a href="#" className="oa-title">
-              {actuality.title}
-            </a>
-            <p className="oa-desc">{actuality.description}</p>
+            <img src={actuality.image} alt={`Actuality ${index + 1}`} />
           </div>
-          <img src={actuality.image} alt={`Actuality ${index + 1}`} />
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
+    </>
   );
 }
